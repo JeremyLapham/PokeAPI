73f3a377b290fdaed920c13a345c1164d26ad7ad
JeremyLapham <jeremylapham2004@gmail.com>
2023-10-17 23:30:09 -0700

    some how managed to find way to make searches more efficient

--- src/Components/DisplayEvolutionChain.js
+++ src/Components/DisplayEvolutionChain.js
@@ -7,7 +7,7 @@ export default function DisplayEvolutionChain({ evoChain, evoClick }) {
             {evoChain?.map(evoChain => {
                 return (
                     <Col style={{ height: 90 }} className='d-flex justify-content-evenly' key={evoChain.id}>
-                        <Button style={{ border: 'none' }} variant='' onClick={() => evoClick(evoChain.id)}>
+                        <Button style={{ border: 'none', cursor: 'pointer' }} variant='' onClick={() => evoClick(evoChain.id)}>
                             <img className='evoBtn' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${evoChain.id}.gif`} alt={'API is not up to date for these images'} />
                             <h5>{evoChain.name}</h5>
                         </Button>
--- src/Services/DataService.js
+++ src/Services/DataService.js
@@ -1,4 +1,3 @@
-let PokeData = '';
 export async function GetPokemonData(pokemon) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const data = await response.json();
@@ -17,40 +16,37 @@ export async function GetPokemonLocation(pokemon) {
     return data;
 }
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function createPokemonObject(species) {
+    return {
+        name: capitalizeFirstLetter(species.name),
+        id: species?.url.split('/').filter(part => part !== '').pop()
+    };
+}
+
 export async function GetPokemonUrl(pokemon) {
     const response1 = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`);
     const data1 = await response1.json();
     const response2 = await fetch(data1.evolution_chain?.url);
     const data2 = await response2.json();
 
-    PokeData = data2;
     let pokemonEvo1 = [];
 
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-
-    function createPokemonObject(species) {
-        return {
-            name: capitalizeFirstLetter(species.name),
-            id: species?.url.split('/').filter(part => part !== '').pop()
-        };
-    }
-
-    if (PokeData.chain.evolves_to.length === 0) {
-        pokemonEvo1.push(createPokemonObject(PokeData.chain.species));
-    } else if (PokeData.chain.evolves_to[0].evolves_to.length > 0) {
-        let pokeArray = [];
-        PokeData.chain.evolves_to.forEach(e => pokeArray.push(e.evolves_to[0].species));
-        pokemonEvo1.push(createPokemonObject(PokeData.chain.species));
-        PokeData.chain.evolves_to.forEach(e => pokemonEvo1.push(createPokemonObject(e.species)));
-        pokeArray.forEach(p => pokemonEvo1.push(createPokemonObject(p)));
+    if (data2.chain.evolves_to.length === 0) {
+        pokemonEvo1.push(createPokemonObject(data2.chain.species));
+    } else if (data2.chain.evolves_to[0].evolves_to.length > 0) {
+        let pokeArray = data2.chain.evolves_to.map(e => e.evolves_to[0].species);
+        pokemonEvo1.push(createPokemonObject(data2.chain.species));
+        pokemonEvo1 = [...pokemonEvo1, ...data2.chain.evolves_to.map(e => createPokemonObject(e.species))];
+        pokemonEvo1 = [...pokemonEvo1, ...pokeArray.map(p => createPokemonObject(p))];
     } else {
-        if (PokeData.chain.evolves_to[0].evolves_to.length <= 0) {
-            pokemonEvo1.push(createPokemonObject(PokeData.chain.species));
-            PokeData.chain.evolves_to.forEach(e => pokemonEvo1.push(createPokemonObject(e.species)));
-        }
+            pokemonEvo1.push(createPokemonObject(data2.chain.species));
+            pokemonEvo1 = [...pokemonEvo1, ...data2.chain.evolves_to.map(e => createPokemonObject(e.species))];
+        
     }
 
-    return pokemonEvo1
+    return pokemonEvo1;
 }
